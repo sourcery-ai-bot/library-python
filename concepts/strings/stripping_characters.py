046e01b26ed262ebdafc1815e37e2fcb67084eec
Wayne Lambert <wayne.a.lambert@gmail.com>
2019-12-01 15:45:14 +0000

    Add example for stripping characters from strings

---
+++ concepts/strings/stripping_characters.py
@@ -0,0 +1,6 @@
+""" Strips given characters from a given sentence. """
+
+sentence_with_lots_of_chars = "Lots of characters here, isn't there!?"
+
+stripped = sentence_with_lots_of_chars.strip(".;?!")
+print(stripped)
