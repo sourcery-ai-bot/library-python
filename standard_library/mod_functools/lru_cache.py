7a27ebaa874c73b5233dcb18250f7dd45e56895e
Wayne Lambert <wayne.a.lambert@gmail.com>
2019-12-03 15:09:16 +0000

    Add functools module

---
+++ standard_library/mod_functools/lru_cache.py
@@ -0,0 +1,19 @@
+from functools import lru_cache
+
+"""
+The `lru_cache` function from the standard `functools` library as a decorator
+to implement memoization (last result caching for re-use on next iteration)
+"""
+@lru_cache(maxsize=1000)
+def fibonacci_recursive(num):
+    if num == 0:
+        return 0
+    elif num == 1:
+        return 1
+    elif num > 1:
+        return fibonacci_recursive(num - 1) + fibonacci_recursive(num - 2)
+
+
+if __name__ == "__main__":
+    for num in range(0, 101):
+        print(f"{'Fib'} {num} {'='} {fibonacci_recursive(num)}")
