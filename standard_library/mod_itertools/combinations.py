cac7d183aae274d8e16b8ec6b8e6d055ff8e7334
Wayne Lambert <wayne.a.lambert@gmail.com>
2019-12-03 22:08:58 +0000

    Separate combinations and permutations into separate files

---
+++ standard_library/mod_itertools/combinations.py
@@ -0,0 +1,14 @@
+import itertools
+from random import randint
+
+letters = ('A', 'B', 'C', 'D', 'E')
+
+# Choose 6 lottery numbers at random
+for i in range(1, 7):
+    num = [randint(1, 59)]
+    if num in num:
+        num = randint(1, 59)
+    print(num)
+
+my_combinations = list(itertools.combinations(letters, 5))
+print(my_combinations)
---
+++ standard_library/mod_itertools/permutations.py
@@ -0,0 +1,17 @@
+import itertools
+from random import randint
+
+letters = ('A', 'B', 'C', 'D', 'E')
+
+# Returns 120 permutations since 5 letters means there are 5! permutations
+print(len(list(itertools.permutations(letters))))
+
+# Choose 6 lottery numbers at random
+for i in range(1, 7):
+    num = [randint(1, 59)]
+    if num in num:
+        num = randint(1, 59)
+    print(num)
+
+my_permutations = list(itertools.permutations(letters, 5))
+print(my_permutations)
